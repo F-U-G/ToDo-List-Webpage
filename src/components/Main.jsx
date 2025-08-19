5cce09b333ed5cd23deb7c6d480ac16038dd59fa
Alistair Merten <alistairmerten@gmail.com>
2025-08-19 17:04:33 -0400

    html and css update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,17 +6,31 @@ export default function Main() {
   }
 
   return (
-    <>
-      <div className="container">
+    <div className="container">
+      <div className="todo-form">
         <form action={addItem}>
-          <label htmlFor="taskName">Task Name:</label>
-          <input type="text" id="taskName" name="taskName"/>
-          <label htmlFor="desc"></label>
-          <input />
+          <div>
+            <label htmlFor="taskName">Task Name:</label>
+            <input type="text" id="taskName" name="taskName"/>
+          </div>
+          <div>
+            <label htmlFor="desc">Task Description:</label>
+            <input type="text" id="taskDesc" name="taskDesc"/>
+          </div>
+          <button>Add</button>
         </form>
       </div>
-      <div className="container">
+      <div className="todo-list">
+        <div className="todo-item">
+          <h4>Test Task</h4>
+          <div className="remove-item" id="0">X</div>
+          <p>
+            This is a description of what needs to be done for this task. 
+            Maybe could be a list option for the user as well? 
+            Im not sure you could do a lot with this element and have a radio button to select the type.
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
