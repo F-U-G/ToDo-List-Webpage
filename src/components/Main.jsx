78bcf912d227dbeb0e5a663b6face1800a7b4440
F-U-G <alistairmerten@gmail.com>
2025-08-24 16:07:03 -0600

    Updated form and list rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react'
+
+let nextID = 0 // id will be used for key later
+
 export default function Main() {
-  function addItem(formData) {
-    //const taskName = formData.get("taskName")
-    //const description = formData.get("description")
-    //const key = *key gen here*
+  const [items, setItems] = useState([])
+
+  function addItem(formData) { // function uses set function to add the form data to the items array
+    const taskName = formData.get("taskName")
+    const taskDesc = formData.get("taskDesc")
+    setItems([...items, { id: nextID++, name: taskName, description: taskDesc}])
+    console.log(nextID)
   }
+
+  // map data for rendering
+  const itemList = items.map(item => {
+    return (
+      <li key={`${item.id}`} className="todo-item">
+        <h4>{item.name}</h4>
+        <div className="remove-item">X</div>
+        <p>{item.description}</p>
+      </li>
+    )
+  })
+
 
   return (
     <div className="container">
@@ -20,16 +39,19 @@ export default function Main() {
           <button>Add</button>
         </form>
       </div>
-      <div className="todo-list">
-        <div className="todo-item">
-          <h4>Test Task</h4>
-          <div className="remove-item" id="0">X</div>
-          <p>
-            This is a description of what needs to be done for this task. 
-            Maybe could be a list option for the user as well? 
-            Im not sure you could do a lot with this element and have a radio button to select the type.
-          </p>
-        </div>
+      <div>
+        <ul className="todo-list">
+          {itemList}
+          {/*<li className="todo-item">
+            <h4>Test Task</h4>
+            <div className="remove-item" id="999">X</div>
+            <p>
+              This is a description of what needs to be done for this task. 
+              Maybe could be a list option for the user as well? 
+              Im not sure you could do a lot with this element and have a radio button to select the type.
+            </p>
+          </li>*/}
+        </ul>
       </div>
     </div>
   )
